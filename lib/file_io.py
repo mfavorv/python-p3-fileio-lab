d6d6ad6c24ef650ec754729fe4b2d7a42d696b3f
Favor Vincent <vfavorm77@gmail.com>
2024-05-31 08:45:44 +0300

    fileio lab complete

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,15 @@
 def write_file(file_name, file_content):
-    pass
+     with open(f"{file_name}.txt", mode="w") as write_file:
+         write_file.write(f"{file_content}")
+pass
 
 def append_file(file_name, append_content):
-    pass
+     with open(f"{file_name}.txt", mode="a") as append_file:
+         append_file.write(f"{append_content}")
+pass
 
-def read_file(file_name):
-    pass
+def read_file(file_name): 
+    with open(f"{file_name}.txt", mode='r') as read_file:
+        content = read_file.read()
+    return content
+pass
